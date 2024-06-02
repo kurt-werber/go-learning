6553bee0b86a77c97dc43f122029fa601fa4bc0d
Kurt W <kurt_w@mail.kurtwerber.com>
2024-06-02 01:16:49 -0400

    up to methods

--- helloworld.go
+++ helloworld.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"runtime"
+	"strings"
 	"time"
 )
 
@@ -12,26 +13,60 @@ type Vertex struct {
 	Y int
 }
 
+const (
+	google    = "google"
+	bell_labs = "bell labs"
+)
+
+var m map[string]Vertex
+
 func main() {
-	a := make([]int, 5, 10)
-	printSlice(a)
-	a = a[:8]
-	printSlice(a)
-	for i := 0; i < 100; i++ {
-		a = append(a, i)
-		printSlice(a)
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
 	}
-	for i, v := range a {
-		fmt.Printf("Index: %d, Value: %d\n", i, v)
+}
+
+func fibonacci() func() int {
+	twoAgo, oneAgo := 0, 1
+	offset := 0
+	return func() int {
+		switch offset {
+		case 0:
+			offset += 1
+			return 0
+		case 1:
+			offset += 1
+			return 1
+		default:
+			sum := twoAgo + oneAgo
+			twoAgo = oneAgo
+			oneAgo = sum
+			return sum
+		}
 	}
-	for _, v := range a {
-		fmt.Printf("Value: %d\n", v)
-	}
+}
+
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
 }
 
 func printSlice(s []int) {
 	fmt.Printf("Length: %d, Capacity: %d\n", len(s), cap(s))
 	fmt.Println(s)
+}
+
+func WordCount(s string) map[string]int {
+	splitString := strings.Fields(s)
+	wordCountMap := make(map[string]int)
+	for _, v := range splitString {
+		if elem, present := wordCountMap[v]; present {
+			wordCountMap[v] = elem + 1
+		} else {
+			wordCountMap[v] = 1
+		}
+	}
+	return wordCountMap
 }
 
 func oldStuff() {
@@ -62,4 +97,42 @@ func oldStuff() {
 	v := Vertex{1, 2}
 	v.X = 4
 	fmt.Println(v.X)
+	a := make([]int, 5, 10)
+	printSlice(a)
+	a = a[:8]
+	printSlice(a)
+	for i := 0; i < 100; i++ {
+		a = append(a, i)
+		printSlice(a)
+	}
+	for i, v := range a {
+		fmt.Printf("Index: %d, Value: %d\n", i, v)
+	}
+	for _, v := range a {
+		fmt.Printf("Value: %d\n", v)
+	}
+	m = make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{40, -74}
+	fmt.Println(m["Bell Labs"])
+
+	mapLiteral := map[string]Vertex{
+		bell_labs: {
+			40, -74,
+		},
+		google: {
+			37, -122,
+		},
+	}
+	fmt.Println(mapLiteral)
+
+	delete(mapLiteral, google)
+	elem, ok := mapLiteral[google]
+	fmt.Printf("The key %s is in the map: %t. Therefore its value is %v\n", google, ok, elem)
+
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	fmt.Println(hypot(5, 12))
+	fmt.Println(compute(hypot))
+	fmt.Println(compute(math.Pow))
 }
